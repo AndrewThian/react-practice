4f3368129baf6231e3e6f0f788942b2a2e94f3e1
Andrew Thian <andrew@buuuk.com>
2018-02-21 10:54:29 +0800

    git update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
-import { Person } from './Person/Person'
+import Person from './Person/Person'
 import './App.css'
 
 class App extends Component {
 
   state = {
     persons: [
-      { name: 'testing123', age: 12 },
-      { name: 'testing431', age: 22 },
-      { name: 'testing231', age: 14 }
+      { name: 'testing123', age: 12, id: 1 },
+      { name: 'testing431', age: 22, id: 2 },
+      { name: 'testing231', age: 14, id: 3 }
     ],
     displayPersons: false
   }
 
+  
   togglePersonsHandler = () => {
     const showPersons = this.state.displayPersons
     this.setState({
@@ -20,21 +21,83 @@ class App extends Component {
     })
   }
 
+  // flaw of this is that state is now mutable
+  // deletePersonHandler = event => {
+  //   return personIndex => {
+  //     const persons = this.state.persons
+  //     persons.splice(personIndex, 1)
+  //     this.setState({ persons: persons })
+  //   }
+  // }
+
+  // always copy a new set of state
+  // return personIndex function to event when it's called
+  deletePersonHandler = personIndex => event => {
+    // slice returns a new array
+    // const persons = this.state.persons.slice()
+    // es6 new syntax spread operator copys and returns a new array
+    const persons = [...this.state.persons]
+    // immutable fashion of setting state
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
+  }
+
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+    const person = { ...this.state.persons[personIndex] }
+    const persons = [...this.state.persons]
+    person.name = event.target.value
+    persons[personIndex] = person
+    this.setState({ persons: persons })
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'red',
+      color: 'white'
+    }
+
+    let persons = null
+
+    if (this.state.displayPersons) {
+      style.backgroundColor = 'green'
+      persons = (
+        <div>
+          {this.state.persons.map((person, ind) => {
+            return <Person
+              personIndex={ind}
+              click={this.deletePersonHandler}
+              changed={e => this.nameChangeHandler(e, person.id)}
+              name={person.name}
+              age={person.age}
+              key={person.id} />
+          })}
+        </div>
+      )
+    }
+    
+    let classes = []
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red')
+    } 
+
+    if (this.state.persons.length <= 1) {
+      classes.push('bold')
+    }
+
     return (
       <div className='App'>
+        <p className={classes.join(' ')}>test dynamic class change</p>
         <button
-        onClick={this.togglePersonsHandler}>
+          style={style}
+          onClick={this.togglePersonsHandler}>
           Click me to toggle Persons
         </button>
         {/* conditionals only with ternary conditionals */}
-        { this.state.displayPersons ? 
-          <div>
-            <Person></Person>
-            <Person></Person>
-            <Person></Person>
-          </div> : null
-        }
+        {persons}
       </div>
     )
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import './Person.css'
 
-export const Person = (props) => {
+const Person = (props) => {
+
+	const handleDelete = (event) => (params => { props.click(params)(event) })
+
 	return (
-		<div>
+		<div className="Person">
 			<hr/>
-			<p>my name is: {props.name}, age is: {props.age}</p>
+			<p
+				// 1. when the page re-renders/renders the handleDelete is executed
+				// 2. the IIFE sets up a reference, then stopped because of a missing event parameter
+				// 3. manually invoked via the click event
+				// 4. pass params to the callback.
+				// refer to this: https://www.udemy.com/react-the-complete-guide-incl-redux/learn/v4/questions/3578426
+				onClick={handleDelete(props.personIndex)}>
+				my name is: {props.name}, age is: {props.age}
+			</p>
+			<input 
+				onChange={props.changed}
+				value={props.name}
+				type="text"/>
 			<hr/>
 		</div>
 	)
 }
+
+export default Person
